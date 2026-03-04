3319c0eb93667d6862e71162ce5507cfec8ef9f1
null_ong2 <jannhyunsoo@gmail.com>
2026-03-05 00:09:09 +0900

    rtk-query

---
+++ shared/providers/StoreProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "@/shared/store/store";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export const StoreProvider = ({ children }: Props) => {
+    return <Provider store={store}>{children}</Provider>;
+};
+
---
+++ shared/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { gitApi } from "@/shared/api/rtk/gitApi";
+
+export const store = configureStore({
+    reducer: {
+        [gitApi.reducerPath]: gitApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(gitApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
